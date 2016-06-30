05dd537b7c5e11a5ecec0122455a1efbd53e767f
Richard <jaylahill@richards-macbook-air.local>
2016-06-30 10:54:31 -0400

    Initial Commit

---
+++ Fraternity app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fraternity app
+//
+//  Created by Richard on 6/30/16.
+//  Copyright © 2016 Richard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fraternity appTests/Fraternity_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fraternity_appTests.swift
+//  Fraternity appTests
+//
+//  Created by Richard on 6/30/16.
+//  Copyright © 2016 Richard. All rights reserved.
+//
+
+import XCTest
+@testable import Fraternity_app
+
+class Fraternity_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
