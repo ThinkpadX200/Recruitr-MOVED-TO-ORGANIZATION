6ae7e3786936e9373625ed782cd39c9f4b616b1d
Richard <jaylahill@richards-macbook-air.local>
2016-07-05 16:19:51 -0400

    Initial Commit

---
+++ Fraternity app/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  Fraternity app
+//
+//  Created by Richard on 6/30/16.
+//  Copyright © 2016 Richard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
